7208799fbe75d7c79600df2b5e381ba1eb8395a7
Allanpt <jatallantavares@gmail.com>
2024-06-01 11:17:46 -0300

    Initial commit

---
+++ src/br/com/javaIndra/Principal.java
@@ -0,0 +1,20 @@
+package br.com.javaIndra;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		System.out.println("---------------");
+		System.out.println("Operadores");
+		
+		boolean condicao = false;
+		
+		if(condicao == true) {
+			System.out.println("true");
+		} else { 
+			System.out.println("false");
+		}
+	}
+
+}
